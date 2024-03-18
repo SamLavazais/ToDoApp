494d2d2c0d3627f7d0bf480a6fe4f72ae58b3b67
SamSam <samuel.lavazais@hotmail.fr>
2024-03-18 18:02:25 +0100

    first postgresql query (getTasks controller)

--- backend/controllers/tasksControllers.js
+++ backend/controllers/tasksControllers.js
@@ -1,6 +1,16 @@
+const pgp = require("pg-promise")(/* options */);
+const db = pgp(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`);
+
 const getTasks = (req, res, next) => {
     try {
-        res.status(200).json("voici la liste des tâches demandées !");
+        db.any("SELECT * from Tasks")
+            .then((data) => {
+                console.log("DATA:", data);
+                res.status(200).json(data);
+            })
+            .catch((error) => {
+                console.log("ERROR:", error);
+            });
     } catch (err) {
         next({
             message: "Impossible de récupérer les tâches demandées",
